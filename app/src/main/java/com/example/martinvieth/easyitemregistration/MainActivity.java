819bc66cc0189cee19704e1c53921e45446fe51b
Martin Vieth <martin_123_vieth@hotmail.com>
2015-11-05 16:19:30 +0100

    Arbejder nu med at tilføje knapper til søg og menu

--- app/src/main/java/com/example/martinvieth/easyitemregistration/MainActivity.java
+++ app/src/main/java/com/example/martinvieth/easyitemregistration/MainActivity.java
@@ -4,15 +4,26 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+
+    ImageButton MenuButton;
+    ImageButton SearchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+
+        MenuButton = (ImageButton) findViewById(R.id.imageButtonMenuIcon);
+        SearchButton = (ImageButton) findViewById(R.id.imageButtonSearch);
+        MenuButton.setImageResource(R.drawable.ic_burgermenu);
+        SearchButton.setImageResource(R.drawable.ic_search);
     }
-    //test commit
 
    
 }
