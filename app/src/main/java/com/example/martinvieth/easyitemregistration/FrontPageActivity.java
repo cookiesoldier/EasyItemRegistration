2916486b20a7e3daf20d1f39832467869a323a8f
Thomas <thomas@liljegren.dk>
2015-11-17 17:20:02 +0100

    Action bar test

--- app/src/main/java/com/example/martinvieth/easyitemregistration/FrontPageActivity.java
+++ app/src/main/java/com/example/martinvieth/easyitemregistration/FrontPageActivity.java
@@ -3,6 +3,7 @@ package com.example.martinvieth.easyitemregistration;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
@@ -31,6 +32,13 @@ public class FrontPageActivity extends Activity implements View.OnClickListener{
 
         cameraFolder.setOnClickListener(this);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //super.onCreateOptionsMenu(menu); // standard menuer
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
 
